523f74e5a8ea996229ed1dad62eb7a30f29e648c
Dito Adhi <adhipradyto32@gmail.com>
2025-09-11 02:25:47 +0800

    add: datatable template

---
+++ frontend/src/pages/transaction/columns.tsx
@@ -0,0 +1,32 @@
+import  { type ColumnDef } from "@tanstack/react-table"
+
+export type Transaction = {
+  amount: number
+  type: "Income" | "Expense"
+  description: string | null
+  category: string
+  date: string
+}
+
+export const columns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+]
