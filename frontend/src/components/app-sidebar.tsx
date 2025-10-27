bd4a7eb410a557fcf1a18ba6914e22dd5f70c68f
Dito Adhi <adhipradyto32@gmail.com>
2025-10-27 22:54:52 +0800

    fe-add: profile in sidebar footer, handle login navigation in main layout, handle logout in sidebar, and several components

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,4 +1,13 @@
-import SpendlyLogo from "/spendly_blacktext.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Select,
   SelectContent,
@@ -9,59 +18,72 @@ import {
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import type { User } from "@/types/User";
+import { faUser, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 import {
-  ChartPie,
-  LayoutDashboard,
   BadgeDollarSign,
+  Bell,
+  ChartPie,
+  Ellipsis,
+  LayoutDashboard,
+  LogOut,
   NotepadText,
   Settings,
+  User as UserIcon,
 } from "lucide-react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import DefaultAvatar from "/profile.png";
+import SpendlyLogo from "/spendly_blacktext.svg";
 
 // Menu items.
 const items = [
   {
     title: "Dashboard",
-    url: "#",
+    url: "/",
     icon: LayoutDashboard,
   },
   {
     title: "Transaction",
-    url: "#",
+    url: "/transaction",
     icon: BadgeDollarSign,
   },
   {
     title: "Chart",
-    url: "#",
+    url: "/chart",
     icon: ChartPie,
   },
   {
     title: "Journal",
-    url: "#",
+    url: "/journal",
     icon: NotepadText,
   },
   {
     title: "Settings",
-    url: "#",
+    url: "/settings",
     icon: Settings,
   },
 ];
 const balance = "512.200.000,00";
 
-export function AppSidebar() {
+export function AppSidebar(user: User) {
+  const { isMobile } = useSidebar();
+  const navigate = useNavigate();
   return (
     <Sidebar>
       <SidebarHeader>
@@ -69,7 +91,7 @@ export function AppSidebar() {
       </SidebarHeader>
       <SidebarContent className="p-3">
         <SidebarGroup>
-          <Select>
+          <Select defaultValue="personal">
             <SelectTrigger className="w-full py-5 bg-primary text-white font-medium [&_svg]:!text-white [&_svg]:stroke-[3] [&_svg]:size-6 [&_svg]:opacity-100">
               <SelectValue />
             </SelectTrigger>
@@ -99,10 +121,13 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem className="my-1" key={item.title}>
-                  <SidebarMenuButton className="py-2" asChild>
+                  <SidebarMenuButton
+                    className="py-2 opacity-70 hover:opacity-100"
+                    asChild
+                  >
                     <a href={item.url}>
-                      <item.icon className="opacity-75" />
-                      <p className="text-base opacity-75">{item.title}</p>
+                      <item.icon />
+                      <p className="text-base">{item.title}</p>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
@@ -111,6 +136,70 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={DefaultAvatar} alt={user.name} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <Ellipsis className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={15}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={DefaultAvatar} alt={user.name} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <UserIcon />
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Bell />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onSelect={async () => {
+                await axios.post(
+                  "http://localhost:5000/api/auth/logout",
+                  {},
+                  { withCredentials: true }
+                );
+                navigate("/login");
+              }}
+            >
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,16 +1,40 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import type { User } from "@/types/User";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+
+  async function fetchUser() {
+    try {
+      const res = await axios.get("http://localhost:5000/api/user/me", {
+        withCredentials: true,
+      });
+      setUser(res.data.user);
+    } catch (err) {
+      console.error(err);
+      navigate("/login");
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
-        <AppSidebar />
+        {user && <AppSidebar {...user} />}
         <main className="flex-1 bg-background">
           <SidebarTrigger />
           {children}
         </main>
       </div>
     </SidebarProvider>
-  )
+  );
 }
