879965f04d30dd531c7050e3ac71664c715cd65a
Dito Adhi <adhipradyto32@gmail.com>
2026-02-08 21:20:38 +0800

    be-add: total spending and total income

--- backend/src/controllers/DashboardController.ts
+++ backend/src/controllers/DashboardController.ts
@@ -2,6 +2,71 @@ import { Request, Response } from "express";
 import prisma from "../prisma/client";
 
 // GET BALANCE FLOW
+export const getIncomeExpense = async (req: Request, res: Response) => {
+  try {
+    const userId = (req as any).user.id;
+    const { month, year } = req.query;
+
+    const startDate = new Date(Number(year), Number(month), 1);
+    const endDate = new Date(Number(year), Number(month) + 1, 1);
+
+    const result = await prisma.transaction.groupBy({
+      by: ["type"],
+      where: {
+        userId: Number(userId),
+        date: {
+          gte: startDate,
+          lt: endDate,
+        },
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    const resultLastMonth = await prisma.transaction.groupBy({
+      by: ["type"],
+      where: {
+        userId: Number(userId),
+        date: {
+          gte: new Date(Number(year), Number(month) - 1, 1),
+          lt: new Date(Number(year), Number(month), 1),
+        },
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    // Calculate rate of change
+    const getPercentageChange = (current: number, previous: number) => {
+      if (previous === 0) return 0;
+      return (((current - previous) / previous) * 100).toFixed(2);
+    };
+
+    res.status(200).json({
+      expense: {
+        total: result.find((r) => r.type === "Expense")?._sum.amount || 0,
+        percentage: getPercentageChange(
+          result.find((r) => r.type === "Expense")?._sum.amount || 0,
+          resultLastMonth.find((r) => r.type === "Expense")?._sum.amount || 0,
+        ),
+      },
+      income: {
+        total: result.find((r) => r.type === "Income")?._sum.amount || 0,
+        percentage: getPercentageChange(
+          result.find((r) => r.type === "Income")?._sum.amount || 0,
+          resultLastMonth.find((r) => r.type === "Income")?._sum.amount || 0,
+        ),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve transactions" });
+  }
+};
+
+// GET BALANCE FLOW
 export const getBalanceFlow = async (req: Request, res: Response) => {
   try {
     const userId = (req as any).user.id;
--- backend/src/routes/dashboard.ts
+++ backend/src/routes/dashboard.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getBalanceFlow } from "../controllers/DashboardController";
+import {
+  getBalanceFlow,
+  getIncomeExpense,
+} from "../controllers/DashboardController";
 import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
 router.get("/balance-flow", authMiddleware, getBalanceFlow);
+router.get("/income-expense", authMiddleware, getIncomeExpense);
 
 export default router;
