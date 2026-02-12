145401dd185745d4ae58196c8054a6e09a0bf1db
Dito Adhi <adhipradyto32@gmail.com>
2026-02-12 22:19:01 +0800

    be-add: get spending and income amount by category

--- backend/src/controllers/DashboardController.ts
+++ backend/src/controllers/DashboardController.ts
@@ -91,3 +91,46 @@ export const getBalanceFlow = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Failed to retrieve transactions" });
   }
 };
+
+// GET SPENDING BY CATEGORY
+export const getSpendingIncomeByCategory = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const userId = (req as any).user.id;
+    const { month, year } = req.query;
+
+    const startDate = new Date(Number(year), Number(month), 1);
+    const endDate = new Date(Number(year), Number(month) + 1, 1);
+
+    const spendingByCategory = await prisma.$queryRaw`
+      SELECT LOWER("Category".name) AS category, SUM("Transaction".amount) AS amount
+      FROM "Transaction"
+      INNER JOIN "Category" ON "Transaction"."categoryId" = "Category".id
+      WHERE "Transaction"."userId" = ${Number(userId)}
+      AND "Transaction".type = 'Expense'
+      AND date >= ${startDate}
+      AND date < ${endDate}
+      GROUP BY category
+      ORDER BY amount DESC
+    `;
+
+    const incomeByCategory = await prisma.$queryRaw`
+      SELECT LOWER("Category".name) AS category, SUM("Transaction".amount) AS amount
+      FROM "Transaction"
+      INNER JOIN "Category" ON "Transaction"."categoryId" = "Category".id
+      WHERE "Transaction"."userId" = ${Number(userId)}
+      AND "Transaction".type = 'Income'
+      AND date >= ${startDate}
+      AND date < ${endDate}
+      GROUP BY category
+      ORDER BY amount DESC
+    `;
+
+    res.status(200).json({ spendingByCategory, incomeByCategory });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve transactions" });
+  }
+};
--- backend/src/routes/dashboard.ts
+++ backend/src/routes/dashboard.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 import {
   getBalanceFlow,
   getIncomeExpense,
+  getSpendingIncomeByCategory,
 } from "../controllers/DashboardController";
 import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
-router.get("/balance-flow", authMiddleware, getBalanceFlow);
 router.get("/income-expense", authMiddleware, getIncomeExpense);
+router.get("/balance-flow", authMiddleware, getBalanceFlow);
+router.get(
+  "/spending-income-by-category",
+  authMiddleware,
+  getSpendingIncomeByCategory,
+);
 
 export default router;
